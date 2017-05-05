b13cd43a2f90f86acfa85915504ff23ba83fe648
simplesports <simplesports.mail@gmail.com>
2017-05-05 09:15:03 -0600

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,36 @@
+import sys
+from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from Main_GUI import *
+
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent=None):
+        QWidget.__init__(self,parent)
+        MainWindow = QtWidgets.QMainWindow()
+        self.MainUi = Ui_MainWindow()
+        self.MainUi.setupUi(self)
+
+        self.setUpMainUiFunction()
+
+    def setUpMainUiFunction(self):
+        #Here is where all of the button functions will be definded
+        self.MainUi.pushButton_2.clicked.connect(self.windowaction)
+
+    def windowaction(self):
+        sub = QtWidgets.QMdiSubWindow()
+        Load_Input = self.MainUi.Load_Input
+        sub.setWidget(Load_Input)
+        sub.setObjectName("Load_Input_window")
+        sub.setWindowTitle("Load Input")
+        self.MainUi.mdiArea.addSubWindow(sub)
+        sub.show()
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = MainWindow()
+    MainWindow.show()
+    sys.exit(app.exec_())
