abca197f07daeb6ad526aec8efa9e6148574b24c
simplesports <simplesports.mail@gmail.com>
2017-05-10 17:16:45 -0600

    Add files via upload

--- WidgetFuncitons.py
+++ WidgetFuncitons.py
@@ -174,3 +174,108 @@ class VoltageDrop(QWidget):
 
         self.VoltageDropGUI = Ui_VoltageDrop()
         self.VoltageDropGUI.setupUi(self)
+        self.VoltageDropGUI.comboBox_Wire_Type.addItems(["CU", "AL"])
+        self.VoltageDropGUI.comboBox_Load_Type.addItems(["Lighting", "Receptacles", "Motor", "Electrical Heating", "Electrical Misc"])
+        self.VoltageDropGUI.comboBox_Wire_Size.addItems(["14","12","10","8","6","4","3","2","1","1/0","2/0","3/0","4/0","250","300","350","400","500","600","700","750","1000"])
+        self.VoltageDropGUI.comboBox_Wire_Insulation.addItems(["THHN","THWN","THWN-2","THHW","THW","THW-2","XHHW","XHHW-2","XHH","RHH","RHW","RHW-2"])
+        self.VoltageDropGUI.comboBox_Phase.addItems(["1","3"])
+        self.VoltageDropGUI.comboBox_Panel_Voltage.addItems(["120/240V-1PH-3W","240/480-1PH-3W","208Y/120V-3PH-4W","480Y/277-3PH-4W"])
+
+        self.VoltageDropGUI.comboBox_Wire_Insulation.setCurrentIndex(1)
+        self.VoltageDropGUI.comboBox_Wire_Size.setCurrentIndex(1)
+
+        self.setUpVoltageDropUi()
+        self.phaseTotalShow()
+        self.LoadInputs()
+
+
+    def setUpVoltageDropUi(self):
+        self.hideOptions()
+        self.VoltageDropGUI.comboBox_Panel_Voltage.activated[str].connect(self.phaseTotalShow)
+        self.VoltageDropGUI.Button_Show_more_Options.clicked.connect(self.hideShowOptions)
+
+
+    def phaseTotalShow(self):
+
+        if self.VoltageDropGUI.comboBox_Panel_Voltage.currentText() == "120/240V-1PH-3W" or self.VoltageDropGUI.comboBox_Panel_Voltage.currentText() == "240/480-1PH-3W":
+            self.VoltageDropGUI.UserInput_Phase_B.hide()
+            self.VoltageDropGUI.text_Phase_B.hide()
+            self.VoltageDropGUI.text_Phase_A.setText("Phase 1")
+            self.VoltageDropGUI.text_Phase_C.setText("Phase 2")
+        else:
+            self.VoltageDropGUI.UserInput_Phase_B.show()
+            self.VoltageDropGUI.text_Phase_B.show()
+            self.VoltageDropGUI.text_Phase_A.setText("Phase A")
+            self.VoltageDropGUI.text_Phase_C.setText("Phase C")
+
+    def hideShowOptions(self):
+        if self.VoltageDropGUI.Button_Show_more_Options.text()=="Show more Options":
+            self.showOptions()
+            self.VoltageDropGUI.Button_Show_more_Options.setText("Hide Options")
+        else:
+            self.hideOptions()
+            self.VoltageDropGUI.Button_Show_more_Options.setText("Show more Options")
+
+    def hideOptions(self):
+        self.VoltageDropGUI.text_Phase.hide()
+        self.VoltageDropGUI.comboBox_Phase.hide()
+        self.VoltageDropGUI.text_Wire_Size.hide()
+        self.VoltageDropGUI.comboBox_Wire_Size.hide()
+        self.VoltageDropGUI.text_Wire_Insulation.hide()
+        self.VoltageDropGUI.comboBox_Wire_Insulation.hide()
+
+    def showOptions(self):
+        self.VoltageDropGUI.text_Phase.show()
+        self.VoltageDropGUI.comboBox_Phase.show()
+        self.VoltageDropGUI.text_Wire_Size.show()
+        self.VoltageDropGUI.comboBox_Wire_Size.show()
+        self.VoltageDropGUI.text_Wire_Insulation.show()
+        self.VoltageDropGUI.comboBox_Wire_Insulation.show()
+
+    def LoadInputs(self):
+
+        try:
+            for i in range(0, len(Functions.Loads['Voltage'])):
+                self.VoltageDropGUI.text_Load_Name = QtWidgets.QLabel(self.VoltageDropGUI.scrollAreaWidgetContents)
+                self.VoltageDropGUI.text_Load_Name.setObjectName("text_Load_Name")
+                self.VoltageDropGUI.gridLayout_Load_List.addWidget(self.VoltageDropGUI.text_Load_Name, i, 0, 1, 1)
+                self.VoltageDropGUI.text_Load_Name.setText("Test Load1")
+
+                self.VoltageDropGUI.text_equal = QtWidgets.QLabel(self.VoltageDropGUI.scrollAreaWidgetContents)
+                self.VoltageDropGUI.text_equal.setObjectName("text_equal")
+                self.VoltageDropGUI.gridLayout_Load_List.addWidget(self.VoltageDropGUI.text_equal, i, 1, 1, 1)
+                self.VoltageDropGUI.text_equal.setText("=")
+
+                self.VoltageDropGUI.text_Load_Value = QtWidgets.QLabel(self.VoltageDropGUI.scrollAreaWidgetContents)
+                self.VoltageDropGUI.text_Load_Value.setObjectName("text_Load_Value")
+                self.VoltageDropGUI.gridLayout_Load_List.addWidget(self.VoltageDropGUI.text_Load_Value, i, 2, 1, 1)
+                self.VoltageDropGUI.text_Load_Value.setText("200")
+
+                self.VoltageDropGUI.text_VA = QtWidgets.QLabel(self.VoltageDropGUI.scrollAreaWidgetContents)
+                self.VoltageDropGUI.text_VA.setObjectName("text_VA")
+                self.VoltageDropGUI.gridLayout_Load_List.addWidget(self.VoltageDropGUI.text_VA, i, 3, 1, 1)
+                self.VoltageDropGUI.text_VA.setText("VA X")
+
+                self.VoltageDropGUI.UserInput_Num_Load = QtWidgets.QLineEdit(self.VoltageDropGUI.scrollAreaWidgetContents)
+                self.VoltageDropGUI.UserInput_Num_Load.setObjectName("UserInput_Num_Load")
+                self.VoltageDropGUI.gridLayout_Load_List.addWidget(self.VoltageDropGUI.UserInput_Num_Load, i, 4, 1, 1)
+
+                self.VoltageDropGUI.text_at = QtWidgets.QLabel(self.VoltageDropGUI.scrollAreaWidgetContents)
+                self.VoltageDropGUI.text_at.setObjectName("text_at")
+                self.VoltageDropGUI.gridLayout_Load_List.addWidget(self.VoltageDropGUI.text_at, i, 5, 1, 1)
+                self.VoltageDropGUI.text_at.setText("at")
+
+                self.VoltageDropGUI.text_Load_Voltage = QtWidgets.QLabel(self.VoltageDropGUI.scrollAreaWidgetContents)
+                self.VoltageDropGUI.text_Load_Voltage.setObjectName("text_Load_Voltage")
+                self.VoltageDropGUI.gridLayout_Load_List.addWidget(self.VoltageDropGUI.text_Load_Voltage, i, 6, 1, 1)
+                self.VoltageDropGUI.text_Load_Voltage.setText("120")
+
+                self.VoltageDropGUI.text_Voltage = QtWidgets.QLabel(self.VoltageDropGUI.scrollAreaWidgetContents)
+                self.VoltageDropGUI.text_Voltage.setObjectName("text_Voltage")
+                self.VoltageDropGUI.gridLayout_Load_List.addWidget(self.VoltageDropGUI.text_Voltage, i, 7, 1, 1)
+                self.VoltageDropGUI.text_Voltage.setText("V")
+
+        except:
+            # add message box to alert the user they need to add loads first
+
+            pass
