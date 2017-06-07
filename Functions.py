86bbb87184d1f3fcf7fa598af74cb9d0ca5ecbe5
simplesports <simplesports.mail@gmail.com>
2017-06-07 14:52:30 -0600

    Delete Functions.py

--- Functions.py
+++
@@ -1,7 +0,0 @@
-import math
-#This is where I will define all the Global veriables
-global filename
-global Loads
-Loads = {'LoadName' : [], 'Voltage': [], 'Wattage': [], 'PF': [], 'VA': [], 'Utility':[]}
-global wireSizeCircularMill
-wireSizeCircularMill = [4110,6530,10380,16510,26240,41740,52620,66360,83690,105600,133100,167800,211600,250000,300000,350000,400000,500000,600000,700000,750000,1000000]
